efa6f6b75d25122c48542ec412df95667a8fc852
William Wallace <me@wiox.me>
2014-08-24 01:02:38 +0100

    inisheaial

---
+++ premake5.lua
@@ -0,0 +1,61 @@
+assert(os.getenv("SOURCE_SDK"), "SOURCE_SDK environmental variable not set")
+
+local SOURCE_SDK = os.getenv("SOURCE_SDK") .. "/mp/src"
+
+solution "gm_stringtable"
+	language "C++"
+	location "project"
+	targetdir "bin"
+
+	flags { "StaticRuntime" }
+
+	includedirs {
+		"include",
+		path.join(SOURCE_SDK, "common"),
+		path.join(SOURCE_SDK, "public"),
+		path.join(SOURCE_SDK, "public/tier0"),
+		path.join(SOURCE_SDK, "public/tier1")
+	}
+
+	libdirs {
+		path.join(SOURCE_SDK, "lib/public")
+	}
+
+	links {
+		"tier0",
+		"tier1"
+	}
+	
+
+	configurations {
+		"Debug",
+		"Release"
+	}
+
+	configuration "Debug"
+		flags { "symbols" }
+
+	configuration "Release"
+		optimize "On"
+
+	configuration "vs*"
+		linkoptions { "/NODEFAULTLIB:LIBCMTD" }
+
+
+	project "gm_stringtable"
+		kind "SharedLib"
+		
+		files {
+			"src/**.cpp",
+			"src/**.h"
+		}
+
+		defines { "GMMODULE" }
+
+		if os.is "windows" then
+			targetsuffix "_win32"
+		elseif os.is "linux" then
+			targetsuffix "_linux"
+		elseif os.is "macosx" then
+			targetsuffix "_osx"
+		end
---
+++ src/main.cpp
@@ -0,0 +1,257 @@
+#include <Windows.h>
+
+#include <string>
+#include <sstream>
+
+#include "GarrysMod/Lua/Interface.h"
+#include "interface.h"
+#include "networkstringtabledefs.h"
+
+using namespace GarrysMod;
+
+const int STRINGTABLE_META = 124571;
+
+INetworkStringTableContainer *networktablecontainer;
+
+int GetLuaFiles(lua_State *state) {
+	INetworkStringTable *client_lua_files = networktablecontainer->FindTable("client_lua_files");
+
+	LUA->CreateTable();
+
+	if (!client_lua_files) {
+		return 1;
+	}
+
+	for (int i = 1; i < client_lua_files->GetNumStrings(); i++){ // Skip first entry
+		LUA->PushNumber(i);
+		LUA->PushString(client_lua_files->GetString(i));
+		LUA->SetTable(-3);
+	}
+
+	return 1;
+}
+
+int GetLuaFileCRC(lua_State *state) {
+	INetworkStringTable *client_lua_files = networktablecontainer->FindTable("client_lua_files");
+	
+	if (!client_lua_files) {
+		return 0;
+	}
+
+	const char *file_name = LUA->CheckString(1);
+	int file_index = client_lua_files->FindStringIndex(file_name);
+
+	if (file_index == INVALID_STRING_INDEX) {
+		return 0;
+	}
+
+	int file_len;
+
+	unsigned int file_crc = *(unsigned int*)client_lua_files->GetStringUserData(file_index, &file_len);
+
+	LUA->PushNumber(file_crc);
+
+	return 1;
+}
+
+//
+// stringtable library functions
+//
+int stringtable_Get(lua_State *state) {
+	INetworkStringTable *table = nullptr;
+
+	switch (LUA->GetType(-1)) {
+		case Lua::Type::STRING:
+			table = networktablecontainer->FindTable(LUA->GetString(1));
+			break;
+		case Lua::Type::NUMBER:
+			table = networktablecontainer->GetTable(LUA->GetNumber(1) - 1); // -1 to make stringtables seem indexed from 1 for PUNY LUA USERS
+			break;
+	}
+
+	if (!table)
+		return 0;
+
+	INetworkStringTable **ud = (INetworkStringTable **)LUA->NewUserdata(sizeof(INetworkStringTable*)); // Y is there no proper userdata binding gAARRY?
+		*ud = table;
+
+		LUA->CreateMetaTableType("StringTable", STRINGTABLE_META);
+	LUA->SetMetaTable(-2);
+
+	return 1;
+}
+
+int stringtable_Count(lua_State *state) {
+	LUA->PushNumber(networktablecontainer->GetNumTables());
+
+	return 1;
+}
+
+//
+// StringTable Meta-Methods
+//
+int StringTable_tostring(lua_State *state) {
+	INetworkStringTable *table = *(INetworkStringTable **)LUA->GetUserdata(1);
+
+	if (!table)
+		return 0;
+
+	std::ostringstream stream;
+	stream << "StringTable [" << table->GetTableId() + 1 << "][" << table->GetTableName() << "]";
+
+	std::string ret = stream.str();
+
+	LUA->PushString(ret.c_str());
+
+	return 1;
+}
+
+int StringTable_GetName(lua_State *state) {
+	INetworkStringTable *table = *(INetworkStringTable **)LUA->GetUserdata(1);
+
+	if (!table)
+		return 0;
+
+	LUA->PushString(table->GetTableName());
+
+	return 1;
+}
+
+int StringTable_GetString(lua_State *state) {
+	INetworkStringTable *table = *(INetworkStringTable **)LUA->GetUserdata(1);
+
+	if (!table)
+		return 0;
+
+	int i = LUA->CheckNumber(2);
+
+	LUA->PushString(table->GetString(i - 1));
+	
+	return 1;
+}
+
+int StringTable_GetUserData(lua_State *state) {
+	INetworkStringTable *table = *(INetworkStringTable **)LUA->GetUserdata(1);
+
+	if (!table)
+		return 0;
+
+	int i = LUA->CheckNumber(2);
+	int size;
+
+	const void *data = table->GetStringUserData(i - 1, &size);
+
+	if (!data)
+		return 0;
+
+	LUA->PushString((const char *)data, size);
+
+	return 1;
+}
+
+int StringTable_GetUserDataInt(lua_State *state) {
+	INetworkStringTable *table = *(INetworkStringTable **)LUA->GetUserdata(1);
+
+	if (!table)
+		return 0;
+
+	int i = LUA->CheckNumber(2);
+	int size;
+
+	const void *data = table->GetStringUserData(i - 1, &size);
+
+	if (!data)
+		return 0;
+
+	LUA->PushNumber(*(unsigned int*)data);
+
+	return 1;
+}
+
+int StringTable_Count(lua_State *state) {
+	INetworkStringTable *table = *(INetworkStringTable **)LUA->GetUserdata(1);
+
+	if (!table)
+		return 0;
+
+	LUA->PushNumber(table->GetNumStrings());
+
+	return 1;
+}
+
+int StringTable_Size(lua_State *state) {
+	INetworkStringTable *table = *(INetworkStringTable **)LUA->GetUserdata(1);
+	
+	if (!table)
+		return 0;
+
+	LUA->PushNumber(table->GetMaxStrings());
+
+	return 1;
+}
+
+
+GMOD_MODULE_OPEN() {
+	LUA->PushSpecial(Lua::SPECIAL_GLOB);
+
+		LUA->GetField(-1, "CLIENT");
+			bool CLIENT = LUA->GetBool();
+		LUA->Pop();
+
+		if (CLIENT) {
+			networktablecontainer = GetInterface<INetworkStringTableContainer *>("engine", INTERFACENAME_NETWORKSTRINGTABLECLIENT);
+		} else {
+			networktablecontainer = GetInterface<INetworkStringTableContainer *>("engine", INTERFACENAME_NETWORKSTRINGTABLESERVER);
+		}
+
+		if (!networktablecontainer) {
+			LUA->ThrowError("(GM_STRINGTABLE) Could not find INetworkStringTableContainer\n");
+			return 0;
+		}
+
+		LUA->CreateTable();
+			LUA->PushCFunction(stringtable_Get);
+			LUA->SetField(-2, "Get");
+
+			LUA->PushCFunction(stringtable_Count);
+			LUA->SetField(-2, "Count");
+		LUA->SetField(-2, "stringtable");
+
+		LUA->CreateMetaTableType("StringTable", STRINGTABLE_META);
+			LUA->Push(-1);
+			LUA->SetField(-2, "__index");
+
+			LUA->PushCFunction(StringTable_tostring);
+			LUA->SetField(-2, "__tostring");
+
+			LUA->PushString("StringTable");
+			LUA->SetField(-2, "__type");
+
+			LUA->PushCFunction(StringTable_GetName);
+			LUA->SetField(-2, "GetName");
+
+			LUA->PushCFunction(StringTable_GetString);
+			LUA->SetField(-2, "GetString");
+
+			LUA->PushCFunction(StringTable_GetUserData);
+			LUA->SetField(-2, "GetUserData");
+
+			LUA->PushCFunction(StringTable_GetUserDataInt);
+			LUA->SetField(-2, "GetUserDataInt");
+
+			LUA->PushCFunction(StringTable_Count);
+			LUA->SetField(-2, "Count");
+
+			LUA->PushCFunction(StringTable_Size);
+			LUA->SetField(-2, "Size");
+		LUA->Pop();
+
+	LUA->Pop();
+	
+
+	return 0;
+}
+
+GMOD_MODULE_CLOSE() {
+	return 0;
+}
