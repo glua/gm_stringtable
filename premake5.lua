df3660a80a903148d9f4506aa1ea679971d088af
Jamie Hankins <jamiehankin@gmail.com>
2014-09-04 19:33:29 +0100

    Fix the build for the build agent

--- premake5.lua
+++ premake5.lua
@@ -1,56 +1,17 @@
-assert(os.getenv("SOURCE_SDK"), "SOURCE_SDK environmental variable not set")
+dofile("../../common.lua")
 
-local SOURCE_SDK = os.getenv("SOURCE_SDK") .. "/mp/src"
+RequireDefaultlibs()
 
-solution "gm_stringtable"
-	language "C++"
-	location "project"
-	targetdir "bin"
+SOLUTION 	"gm_stringtable"
 
-	flags { "StaticRuntime" }
+targetdir	"bins"
+INCLUDES	"source_sdk"
+INCLUDES 	"gmod_sdk"
+defines		{"NDEBUG"}
 
-	includedirs {
-		"include",
-		path.join(SOURCE_SDK, "common"),
-		path.join(SOURCE_SDK, "public"),
-		path.join(SOURCE_SDK, "public/tier0")
-	}
-
-	libdirs {
-		path.join(SOURCE_SDK, "lib/public")
-	}
-
-	links {
-		"tier0"
-	}
-	
-
-	configurations {
-		"Debug",
-		"Release"
-	}
-
-	configuration "Debug"
-		flags { "symbols" }
-
-	configuration "Release"
-		optimize "On"
-
-
-	project "gm_stringtable"
-		kind "SharedLib"
-		
-		files {
-			"src/**.cpp",
-			"src/**.h"
-		}
-
-		defines { "GMMODULE" }
-
-		if os.is "windows" then
-			targetsuffix "_win32"
-		elseif os.is "linux" then
-			targetsuffix "_linux"
-		elseif os.is "macosx" then
-			targetsuffix "_osx"
-		end
+WINDOWS()
+LINUX()
+PROJECT()
+SOURCE_SDK_LINKS()
+configuration 		"windows"
+configuration 		"linux"
